51708f45027001e822eb19490f683ad6cc4649e3
Aparna Priyavadan <aparnapri@Aparnas-MacBook-Pro.local>
2023-05-01 17:38:36 -0400

    implememted users functionality

--- db/users.js
+++ db/users.js
@@ -1,39 +1,80 @@
 const client = require("./client");
-const bcrypt = require('bcrypt');
+
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-
   try {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    
-    const {rows :[user]} = await client.query(`
+
+    const {rows: [user]} = await client.query(`
+
     INSERT INTO users(username, password)
     VALUES ($1,$2)
     ON CONFLICT (username) DO NOTHING
     RETURNING *;
-  `, [username, hashedPassword]);
 
-  return user;
-} catch (error) {
-  throw error;
-}
-  
+  `,[username, hashedPassword]);
+    //removing the password so it is not returned
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUser({ username, password }) {
+  try{
 
+      const user = await getUserByUsername(username);
+      const hashedPassword = user.password;
+
+      const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+
+      if (passwordsMatch) {
+        delete user.password;
+        return user;
+      } else {
+        throw new Error; //create a new error object InvalidPassword?
+      }
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+  
 }
 
 async function getUserById(userId) {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT *
+    FROM users
+    WHERE id=$1;
+  `, [userId]);
+  delete user.password;
+  return user;
+  } catch (error) {
+    console.log(error);
+  }
 
 }
 
 async function getUserByUsername(userName) {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT *
+    FROM users
+    WHERE username=$1;
+  `, [userName]);
 
+  return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
@@ -41,4 +82,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
