767499c2b3c7c3fe8588f1e2d4980e341b8ce465
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-05-01 18:04:26 -0400

    Finished activites test

--- db/activities.js
+++ db/activities.js
@@ -5,7 +5,9 @@ async function createActivity({ name, description }) {
   // return the new activity
   try {
     // console.log("Creating activities!");
-    const { rows } = await client.query(
+    const {
+      rows: [activity],
+    } = await client.query(
       `
     INSERT INTO activities (name, description)
     VALUES($1,$2)
@@ -13,10 +15,10 @@ async function createActivity({ name, description }) {
     `,
       [name, description]
     );
-    console.log(rows);
-    return rows;
+
+    return activity;
   } catch (error) {
-    throw new Error();
+    throw error;
   }
 }
 
@@ -28,22 +30,77 @@ async function getAllActivities() {
        FROM activities;
      `
     );
-    console.log("activities", rows);
     return rows;
   } catch (error) {
     throw error;
   }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+    SELECT * 
+    FROM activities
+    WHERE id = $1
+    `,
+      [id]
+    );
 
-async function getActivityByName(name) {}
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getActivityByName(name) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+    SELECT * 
+    FROM activities
+    WHERE name = $1
+    `,
+      [name]
+    );
+
+    return activity;
+  } catch (error) {}
+}
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {}
 
 async function updateActivity({ id, ...fields }) {
   // don't try to update the id
+
+  try {
+    const setString = Object.keys(fields)
+      .map((key, index) => `${key}=$${index + 1}`)
+      .join(", ");
+
+    if (setString.length > 0) {
+      const {
+        rows: [updateActivity],
+      } = await client.query(
+        `
+      UPDATE activities
+      SET ${setString}
+      WHERE id = ${id}
+      RETURNING *;
+      `,
+        Object.values(fields)
+      );
+      console.log(updateActivity);
+      return updateActivity;
+    }
+  } catch (error) {
+    throw error;
+  }
   // do update the name and description
   // return the updated activity
 }
