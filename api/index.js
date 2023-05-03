e6a6d50ad20d6eefd2beba1e546d578aaa8cc0a7
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-05-03 11:08:31 -0400

    Implement req.user and error handling

--- api/index.js
+++ api/index.js
@@ -1,25 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const { getUserById } = require("../db");
+
+//Check token and establish our req.user
+router.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      req.user = await getUserById(id);
+      next();
+    } catch ({ name }) {
+      next({
+        name,
+      });
+    }
+  } else {
+    next({ name: "Authorization Error" });
+  }
+});
+router.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set: ", req.user);
+  }
+  next();
+});
 
 // GET /api/health
-router.get('/health', async (req, res, next) => {
-    res.send("Hello");
+router.get("/health", async (req, res, next) => {
+  res.send("Hello");
 });
 
 // ROUTER: /api/users
-const usersRouter = require('./users');
-router.use('/users', usersRouter);
+const usersRouter = require("./users");
+router.use("/users", usersRouter);
 
 // ROUTER: /api/activities
-const activitiesRouter = require('./activities');
-router.use('/activities', activitiesRouter);
+const activitiesRouter = require("./activities");
+router.use("/activities", activitiesRouter);
 
 // ROUTER: /api/routines
-const routinesRouter = require('./routines');
-router.use('/routines', routinesRouter);
+const routinesRouter = require("./routines");
+router.use("/routines", routinesRouter);
 
 // ROUTER: /api/routine_activities
-const routineActivitiesRouter = require('./routineActivities');
-router.use('/routine_activities', routineActivitiesRouter);
+const routineActivitiesRouter = require("./routineActivities");
+router.use("/routine_activities", routineActivitiesRouter);
+
+//Error Handler
+router.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+  });
+});
 
 module.exports = router;
