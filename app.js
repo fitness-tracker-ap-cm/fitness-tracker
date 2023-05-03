069a9e0adb71813ad8943a8096d2402756043247
Aparna Priyavadan <aparnapri@Aparnas-MacBook-Pro.local>
2023-05-03 10:27:58 -0400

    setup api server

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
+const { client } = require('./db');
 
-// Setup your Middleware and API Router here
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//It is a middleware for logging, use morgan for logging server requets and responses etc. could be 'dev' or 'tiny' or 'common' 
+
+app.use(morgan('dev'));
+
+// API Router here
+const apiRouter = require('./api');
+
+// client.connect();
+
+
+
+
+app.use('/api',apiRouter);
 
 module.exports = app;
