45dc85bac62bd19e24c730280ad78e5d08b53dbb
Aparna Priyavadan <aparnapri@Aparnas-MacBook-Pro.local>
2023-05-04 10:47:13 -0400

    routine and user functions updated

--- api/utils.js
+++ api/utils.js
@@ -1,9 +1,12 @@
+const { UnauthorizedError} = require("../errors");
+
 function requireUser(req, user, next) {
   //Check if there is a user logged in
   if (!req.user) {
     next({
-      name: "MissingUserError",
-      message: "You must be logged in to perform this action",
+      message: UnauthorizedError(),
+        name: "UnauthorizedError",
+        error: "UnauthorizedError"
     });
   }
   next();
