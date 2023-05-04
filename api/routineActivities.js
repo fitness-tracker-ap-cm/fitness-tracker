dbc17369492b644f6d36d0886f3f3c31ce99047e
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-05-04 14:20:57 -0400

    routine_activities

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,7 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { requireUser } = require("./utils");
+const {
+  updateRoutineActivity,
+  getRoutineActivitiesByRoutine,
+  canEditRoutineActivity,
+  getRoutineActivityById,
+} = require("../db");
 
 // PATCH /api/routine_activities/:routineActivityId
+router.patch("/:routineActivityId", requireUser, async (req, res, next) => {
+  const id = +req.params.routineActivityId;
+  const routine_activity = await getRoutineActivityById(id);
+  console.log(routine_activity);
+});
 
 // DELETE /api/routine_activities/:routineActivityId
 
