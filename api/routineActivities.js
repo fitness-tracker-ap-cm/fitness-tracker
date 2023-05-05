9661307e8f2451851b40136f276e7bdf06c6100c
Aparna Priyavadan <aparnapri@Aparnas-MacBook-Pro.local>
2023-05-05 10:58:51 -0400

    Added 404 and fixed routineActivities

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -16,18 +16,21 @@ const {
 router.patch("/:routineActivityId", requireUser, async (req, res, next) => {
   const id = +req.params.routineActivityId;
   const { count, duration } = req.body;
-  const { name } = await getRoutineById(id);
-  const canEdit = await canEditRoutineActivity(id, req.user.id);
-  if (!canEdit) {
-    next({
-      name: "Unauthorized to make edits",
-      message: UnauthorizedUpdateError(req.user.username, name),
-      error: "Unauthorized to make edits",
-    });
-  }
   try {
-    const result = await updateRoutineActivity({ id, count, duration });
-    res.send(result).status(200);
+    const canEdit = await canEditRoutineActivity(id, req.user.id);
+    if (canEdit) {
+      const result = await updateRoutineActivity({ id, count, duration });
+      res.send(result).status(200);
+    } else {
+      const routine = await getRoutineById(id);
+      if (routine !== null) {
+        next({
+          message: UnauthorizedUpdateError(req.user.username, routine.name),
+          name: "Unauthorized to make edits",
+          error: "Unauthorized to make edits",
+        });
+      }
+    }
   } catch (error) {
     next(error);
   }
@@ -37,26 +40,25 @@ router.patch("/:routineActivityId", requireUser, async (req, res, next) => {
 
 router.delete("/:routineActivityId", requireUser, async (req, res, next) => {
   const id = +req.params.routineActivityId;
-  const canEdit = await canEditRoutineActivity(id, req.user.id);
-  const { name } = await getRoutineById(id);
-
-  if (!canEdit) {
-    res.status(403);
-    next({
-      name: "Unauthorized to delete",
-      message: UnauthorizedDeleteError(req.user.username, name),
-      error: "Unauthorized to delete",
-    });
-  }
-  if (canEdit) {
-    try {
+  try {
+    const canEdit = await canEditRoutineActivity(id, req.user.id);
+    if (canEdit) {
       const deletedRoutineActivity = await destroyRoutineActivity(id);
       res.send(deletedRoutineActivity).status(200);
-    } catch (error) {
-      next(error);
+    } else {
+      const routine = await getRoutineById(id);
+      if (routine !== null) {
+        res.status(403);
+        next({
+          message: UnauthorizedDeleteError(req.user.username, routine.name),
+          name: "Unauthorized to delete",
+          error: "Unauthorized to delete",
+        });
+      }
     }
+  } catch (error) {
+    next(error);
   }
-  console.log(canEdit);
 });
 
 module.exports = router;
