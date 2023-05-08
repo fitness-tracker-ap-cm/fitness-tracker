acf46cc8a2d9ad12114f68782afd434ddd7a26d3
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-05-08 14:56:30 -0400

    Register a user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,2 +1,42 @@
-
+const BASE = "http://localhost:3000/api";
+//********************aparna **************************
+export async function getAllPublicRoutines() {
+  try {
+    const response = await fetch(`${BASE}/routines`);
+    const publicRoutinesList = await response.json();
+    console.log("Routines :", publicRoutinesList);
+    return publicRoutinesList;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
+//*****************************Christian *********************************
+export async function getAllActivities() {
+  try {
+    const response = await fetch(`${BASE}/activities`);
+    const activitiesList = await response.json();
+    console.log("activities : ", activitiesList);
+    return activitiesList;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export const registerUser = async (userObj) => {
+  try {
+    const response = await fetch(`${BASE}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userObj),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,14 +4,43 @@ import { registerUser } from "../api";
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const userObj = {
+      username,
+      password,
+    };
+    try {
+      const { user, token } = await registerUser(userObj);
+      if (token) {
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
-      <form>
-        <label>Username</label>
-        <input placeholder="Enter a username" />
-        <label>Password</label>
-        <input placeholder="Enter a password" />
-        <button>Register</button>
+      <form onSubmit={handleSubmit}>
+        <label>Username: </label>
+        <input
+          placeholder="Enter a username"
+          type="text"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label>Password: </label>
+        <input
+          placeholder="Enter a password"
+          type="text"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Register</button>
       </form>
     </div>
   );
