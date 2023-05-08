097e54abdf2b511e6dc99da61e90ceae139132ef
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-05-08 14:08:26 -0400

    Make register form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { registerUser } from "../api/api";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input placeholder="Enter a username" />
+        <label>Password</label>
+        <input placeholder="Enter a password" />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
